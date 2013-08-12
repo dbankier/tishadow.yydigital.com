173de9c4d7d623e9f40d4e46c88701ec1a307be8
David Bankier <dbankier@hotmail.com>
2013-08-12 12:33:04 +1000

    no fav

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'ejs');
-  app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
